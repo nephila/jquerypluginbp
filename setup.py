7da0828c47871f09065f221a5d2ee49fa1dd2a5f
Andrea Stagi <stagi.andrea@gmail.com>
2015-01-03 14:00:56 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="jquery-pugin-boilerplate",
+      py_modules=['generate'],
+      version="0.1.0",
+      description="Script to generate boilerplate for you jquery plugin",
+      license="MIT",
+      author="Andrea Stagi",
+      author_email="stagi.andrea@gmail.com",
+      url="",
+      keywords= "jquery plugin script boilerplate",
+      zip_safe = True)
