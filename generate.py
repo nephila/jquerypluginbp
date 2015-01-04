60f9ec070c89aa59f1ec89a1041a5d7850c58b3a
Andrea Stagi <stagi.andrea@gmail.com>
2015-01-04 01:22:05 +0100

    New setup.py script

--- generate.py
+++ generate.py
@@ -8,6 +8,12 @@ import sys
 class PackageManifestException(Exception):
     pass
 
+BOILERPLATE = [
+    '.bowerrc', '.gitignore', 'Gruntfile.js', 'LICENSE', 'package.json', 'README.md',
+    'demo/css/main.css', 'demo/index.html', 'src/jquery.{{plugin_name}}.js',
+    'test/index.html', 'test/tests.js'
+]
+
 def parse_package_manifest(package_manifest_json):
     manifest = {}
     parameters = {}
--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from generate import BOILERPLATE
+import os
 
 setup(name="jquery-plugin-boilerplate",
-      py_modules=['generate'],
-      version="0.1.0",
-      description="Script to generate boilerplate for you jquery plugin",
-      license="MIT",
-      author="Andrea Stagi",
-      author_email="stagi.andrea@gmail.com",
-      url="",
-      keywords= "jquery plugin script boilerplate",
-      entry_points = {
+    py_modules=['generate'],
+    version="0.1.0",
+    description="Script to generate boilerplate for you jquery plugin",
+    license="MIT",
+    author="Andrea Stagi",
+    author_email="stagi.andrea@gmail.com",
+    url="",
+    keywords= "jquery plugin script boilerplate",
+    data_files=[(os.path.join('bin', 'boilerplate', os.path.split(boilerfile)[0]),[os.path.join('boilerplate', boilerfile)]) for boilerfile in BOILERPLATE],
+    entry_points = {
         'console_scripts': [
             'jqueryplugin = generate:main',
         ],
-      },
-      zip_safe = True)
+    },
+    zip_safe = True)
