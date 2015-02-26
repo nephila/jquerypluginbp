e14b46ec9045f797d48b134081f02cfd16910445
Andrea Stagi <stagi.andrea@gmail.com>
2015-02-26 08:36:27 +0100

    Add .jshintrc to boilerplate

--- jquerypluginbp/boilerplate.py
+++ jquerypluginbp/boilerplate.py
@@ -1,5 +1,5 @@
 BOILERPLATE = [
-    '.bowerrc', '.gitignore', 'Gruntfile.js', 'LICENSE', 'package.json', 'README.md',
-    'demo/css/main.css', 'demo/index.html', 'src/jquery.{{plugin_name}}.js',
+    '.jshintrc', '.bowerrc', '.gitignore', 'Gruntfile.js', 'LICENSE', 'package.json',
+    'README.md', 'demo/css/main.css', 'demo/index.html', 'src/jquery.{{plugin_name}}.js',
     'test/index.html', 'test/tests.js'
 ]
